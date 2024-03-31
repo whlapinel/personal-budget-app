50b35e97d4014dc3609cb9180434c1517c7c19d0
Will Lapinel <whlapinel@gmail.com>
2024-03-31 15:09:01 -0400

    update transaction method

--- personal-budget-app-backend/models/transaction.go
+++ personal-budget-app-backend/models/transaction.go
@@ -40,3 +40,52 @@ func (t *Transaction) Save() error {
 	fmt.Println("Transaction created and account balance and monthly budget updated for account: ", t.AccountID)
 	return nil
 }
+
+func (t *Transaction) reverseAccountUpdate() error {
+	db := database.InitializeDB()
+	defer db.Close()
+	_, err := db.Exec("CALL update_account_balance(?, ?)", t.AccountID, -t.Amount)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Transaction) reverseMonthlyBudgetUpdate() error {
+	db := database.InitializeDB()
+	defer db.Close()
+	_, err := db.Exec("CALL update_monthly_budget_spent(?, ?, ?, ?)", t.CategoryID, t.Date.Month(), t.Date.Year(), -t.Amount)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ot *Transaction) Update(nt *Transaction) error {
+	// method called on old transaction (ot) with param new transaction (nt)
+	db := database.InitializeDB()
+	defer db.Close()
+	// get previous transaction amount
+	err := db.QueryRow("SELECT amount FROM transactions WHERE id = ?", ot.ID).Scan(&ot.Amount)
+	if ot.Amount != nt.Amount {
+		ot.reverseAccountUpdate()
+		ot.reverseMonthlyBudgetUpdate()
+	}
+	if err != nil {
+		return err
+	}
+	// update transaction
+	_, err = db.Exec("UPDATE transactions SET account_id = ?, date = ?, payee = ?, amount = ?, memo = ?, category_id = ?, email = ? WHERE id = ?", nt.AccountID, nt.Date, nt.Payee, nt.Amount, nt.Memo, nt.CategoryID, nt.Email, nt.ID)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("CALL update_account_balance(?, ?)", nt.AccountID, nt.Amount)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("CALL update_monthly_budget_spent(?, ?, ?, ?)", nt.CategoryID, nt.Date.Month(), nt.Date.Year(), nt.Amount)
+	if err != nil {
+		return err
+	}
+	return nil
+}
